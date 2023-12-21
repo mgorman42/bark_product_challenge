f4ebf7c8b52b991c2ca9f9df8c94190058ba28e0
Michael Gorman <michael.gorman@haulhub.com>
2023-12-21 12:28:00 -0600

    user model with tests and password validation

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  PASSWORD_VALIDATIONS = {
+    contains_lowercase:         /[a-z]+/,
+    contains_uppercase:         /[A-Z]+/,
+    contains_digit:             /\d+/,
+    contains_special_character: /[^\w\d\s]+/
+  }
+  has_secure_password
+  validates :password, length: { minimum: 12 , message: I18n.t("activerecord.errors.models.user.attributes.password.length")}, allow_blank: true
+  validate :validate_password_requirements, if: -> { password.present? }
+
+  def validate_password_requirements
+    PASSWORD_VALIDATIONS.each do |type, regex|
+      errors.add(:password, I18n.t("activerecord.errors.models.user.attributes.password.#{type}")) unless password.match(regex)
+    end
+  end
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  let(:user) { User.new }
+
+  describe 'password' do
+    let(:valid_password)        { "ValidPassword1234!@#" }
+    let(:min_length_password)   { "Ab1@Ab1@Ab1@" }
+    let(:short_password)        { "Ab1@Ab1@Ab1" }
+    let(:no_caps_password)      { "nocapspassword1234!@#" }
+    let(:no_lowercase_password) { "NOLOWERCASE1234!@#" }
+    let(:no_num_password)       { "NoNumberPassword!@#" }
+    let(:no_sybmbol_password)   { "NoSymbolPassword1234" }
+    let(:max_length_password)   { "Ab1@" * 18 }
+    let(:to_long_password)      { "Ab1@" * 20 }
+
+    def set_password(password, password_confirmation)
+      user.password = password
+      user.password_confirmation = password_confirmation
+      user
+    end
+
+    def assert_password_valid(password)
+      set_password(password, password)
+      assert user.valid?
+    end
+
+    def assert_password_invalid(password, error_message, password_confirmation=nil)
+      password_confirmation ||= password
+      set_password(password, password_confirmation)
+      assert_not user.valid?
+      assert_includes  user.errors.messages.values.flatten, error_message
+    end
+
+    it 'validates password' do
+      assert_password_invalid(nil, "Please enter a password.")
+      assert_password_valid(valid_password)
+      assert_password_valid(min_length_password)
+      assert_password_valid(max_length_password)
+      assert_password_invalid(valid_password, "doesn't match Password", "")
+      assert_password_invalid(valid_password, "doesn't match Password", valid_password.reverse )
+      assert_password_invalid(short_password, "must be atleast 12 characters long")
+      assert_password_invalid(no_lowercase_password, "must contain a lowercase letter")
+      assert_password_invalid(no_caps_password, "must contain an uppercase letter")
+      assert_password_invalid(no_num_password, "must contain a number")
+      assert_password_invalid(no_sybmbol_password, "special character")
+      assert_password_invalid(to_long_password, "can't be more than 72 characters long")
+    end
+  end
+end
