120330ec667bb42e97b852309fc653df6d8f3d20
Michael Gorman <michael.gorman@haulhub.com>
2023-12-22 09:56:16 -0600

    removed portions of scaffold that wont be needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     contains_lowercase:         /[a-z]+/,
     contains_uppercase:         /[A-Z]+/,
     contains_digit:             /\d+/,
-    contains_special_character: /[^\w\d\s]+/
+    contains_special_character: /[^\w\d]+/
   }
   has_secure_password
   validates :password, length: { minimum: 12 , message: I18n.t("activerecord.errors.models.user.attributes.password.length")}, allow_blank: true
